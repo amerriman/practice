6f55f30fc4f661b70f75510ff79a55a5bd75169e
amerriman <amm434@gmail.com>
2016-01-25 11:40:46 -0700

    completed 06

--- 06_join_for_loop/src.js
+++ 06_join_for_loop/src.js
@@ -5,7 +5,56 @@
 module.exports = {
 
   joinWithFor: function(list) {
-    // your code here
-  }
+    var result = "";
+    for (var i = 0; i < list.length; i++) {
+      result += list[i];
+    }
+    return result;
 
-}
+  },
+
+  joinWithForAndIndex: function(list) {
+    var result = "";
+    for (var i = 0; i < list.length; i++) {
+      result += list[i] + [i];
+    }
+    return result;
+  },
+
+  joinWithoutOddCharacters: function(list) {
+    var result = "";
+    for (var i = 0; i < list.length; i++) {
+      if ([i] % 2 === 0){
+        result += list[i];
+      }
+    }
+    return result;
+  },
+
+  joinWithForAndToken: function(list, token) {
+    var result = '';
+    for (var i = 0; i < list.length; i++) {
+      if(i === list.length -1){
+        result += list[i];
+      } else {
+        result += list[i] + token;
+      }
+    }
+    return result;
+  },
+
+  joinWithForAndAlternatingTokens: function(list, token1, token2) {
+    var result = '';
+    for (var i = 0; i < list.length; i++) {
+      if(i === list.length-1){
+        result += list[i];
+      } else if(i % 2 === 0){
+        result += list[i] + token1;
+      } else{
+        result += list[i] + token2;
+      }
+    }
+    return result;
+  },
+
+};
