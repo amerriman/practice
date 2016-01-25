bae2ee1f419abf8e226c94ec8d672d85fd18e94e
amerriman <amm434@gmail.com>
2016-01-25 11:16:06 -0700

    04 done

--- 04_objects/src.js
+++ 04_objects/src.js
@@ -1,5 +1,43 @@
 module.exports = {
   person: function() {
-    // your code here
-  }
-}
+    var obj = {
+      firstName: 'John',
+      lastName: 'Mayer'
+    };
+    return obj;
+  },
+
+  sweetnessProperty: function(obj){
+    return obj.sweetness;
+  },
+
+  savorynessProperty: function(obj){
+    return obj['savory-ness'];
+  },
+
+  keys: function(obj){
+    var allKeys = [];
+    for(var key in obj){
+      allKeys.push(key);
+    }
+    return allKeys;
+  },
+
+  sortedKeys: function(obj){
+    var allKeys = [];
+    for(var key in obj){
+      allKeys.push(key);
+    }
+    return allKeys.sort();
+  },
+
+  reverseSortedKeys: function(obj){
+    var allKeys = [];
+    for(var key in obj){
+      allKeys.push(key);
+    }
+    return allKeys.sort().reverse();
+  },
+
+};
+
